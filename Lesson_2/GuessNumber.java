c98c6ab9a375a61918a4eb64f6a94077ab593272
vopratka <100875122+vopratka@users.noreply.github.com>
2022-04-08 13:11:20 +0300

    Исправлено по замечаниям
    Диапазон рандомного числа указан без цикла.
    Исключены условия цикла, цикл преобразован в бесконечный.

--- Lesson_2/GuessNumber.java
+++ Lesson_2/GuessNumber.java
@@ -12,40 +12,33 @@ public class GuessNumber{
         this.player2 = player2;
     }
    
-    public void getStarted() {
+    public void launch() {
         Random random = new Random();
-        int rand = random.nextInt(101);
-        if (rand != 0) {
-            rand = rand;
-        } else {
-            rand = rand + 1;
-        }      
-        do {
-            System.out.println("Число вводит " + player1.getName());
+        int rand = random.nextInt(100) + 1;
+        
+        System.out.println(player1.getName() + ", введи число:");        
+        while (true) {
             player1.setNumber(scan.nextInt());
             if (player1.getNumber() > rand) {
               System.out.println("Данное число больше того, что загадал компьютер");
-              System.out.println("Ход переходит к " + player2.getName());  
             } else if (player1.getNumber() < rand) {
               System.out.println("Данное число меньше того, что загадал компьютер");
-              System.out.println("Ход переходит к " + player2.getName());
             } else {
                 System.out.println("Выиграл игрок " + player1.getName());
                 break;
             }
+            System.out.println(player2.getName() + ", введи число:");
             
-            System.out.println("Число вводит " + player2.getName());
-            player1.setNumber(scan.nextInt());
+            player2.setNumber(scan.nextInt());
             if (player2.getNumber() > rand) {
               System.out.println("Данное число больше того, что загадал компьютер");
-              System.out.println("Ход переходит к " + player1.getName());  
             } else if (player2.getNumber() < rand) {
               System.out.println("Данное число меньше того, что загадал компьютер");
-              System.out.println("Ход переходит к " + player1.getName());
             } else {
                 System.out.println("Выиграл игрок " + player2.getName());
                 break;
             }
-        } while (player1.getNumber() != rand || player2.getNumber() != rand);
+            System.out.println(player1.getName() + ", введи число:");
+        }
     }
 }
